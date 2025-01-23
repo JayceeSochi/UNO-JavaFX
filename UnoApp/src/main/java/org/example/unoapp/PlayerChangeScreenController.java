77b5bf7061065f1e2859d402d07399693b279a0b
Jedistarfighter <lostevens@student.neumont.edu>
2025-01-23 15:28:41 -0700

    Added Player Change screen

---
+++ UnoApp/src/main/java/org/example/unoapp/PlayerChangeScreenController.java
@@ -0,0 +1,18 @@
+package org.example.unoapp;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+
+public class PlayerChangeScreenController {
+    @FXML
+    private Label nextPlayerLbl;
+
+    @FXML
+    private Button readyButton;
+
+    @FXML
+    void StartNextTurn(ActionEvent event) {
+
+    }
+}
