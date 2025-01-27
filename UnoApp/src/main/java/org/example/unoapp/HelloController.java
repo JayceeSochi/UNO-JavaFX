6449635d489c9ea5cd4750da0a699d891c3c4512
Jedistarfighter <lostevens@student.neumont.edu>
2025-01-27 15:30:51 -0700

    Goes to game page after "HelloController"

--- UnoApp/src/main/java/org/example/unoapp/HelloController.java
+++ UnoApp/src/main/java/org/example/unoapp/HelloController.java
@@ -1,9 +1,12 @@
 package org.example.unoapp;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextFormatter;
 import org.controlsfx.control.tableview2.filter.filtereditor.SouthFilter;
 
+import java.io.IOException;
 import java.util.ArrayList;
 
 public class HelloController {
@@ -13,7 +16,7 @@ public class HelloController {
     private Label welcomeText;
 
     @FXML
-    protected void onHelloButtonClick() {
+    protected void onHelloButtonClick(ActionEvent event) throws IOException {
         // This is to test that the number of players desired is passed through.
         welcomeText.setText("Player Count: " + numPlayers);
 
@@ -26,5 +29,7 @@ public class HelloController {
         System.out.println("Deck Size: " + deck.size());
 //
 //        Game.DrawCard();
+
+        ChangeScene.changeScene(event, "game-screen.fxml");
     }
 }
